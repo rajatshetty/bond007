2554ff761f5080d858d6cd05b5880c8c83913cab
Rajat Shetty <rajat_shetty@live.com>
2016-05-24 15:08:53 +0530

    Commit!

--- bond007-App/bond007-App/App_Start/BundleConfig.cs
+++ bond007-App/bond007-App/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace bond007_App
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+             "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -23,9 +23,31 @@ namespace bond007_App
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            /*bundles.Add(new StyleBundle("~/Content/theme/css").Include(
+                     "~/Content/bootstrap/bower_components/bootstrap/dist/css/bootstrap.min.css",
+                     "~/Content/bootstrap/bower_components/metisMenu/dist/metisMenu.min.css",
+                     "~/Content/bootstrap/dist/css/timeline.css",
+                     "~/Content/bootstrap/dist/css/sb-admin-2.css",
+                     "~/Content/bootstrap/bower_components/morrisjs/morris.css",
+                     "~/Content/bootstrap/bower_components/font-awesome/css/font-awesome.min.css"));
+                     */
+
+            bundles.Add(new ScriptBundle("~/bundles/theme").Include(
+               "~/Content/bootstrap/bower_components/jquery/dist/jquery.min.js",
+                "~/Content/bootstrap/bower_components/bootstrap/dist/js/bootstrap.min.js", "~/Content/bootstrap/bower_components/metisMenu/dist/metisMenu.min.js",
+                "~/Content/bootstrap/bower_components/raphael/raphael-min.js",
+    "~/Content/bootstrap/bower_components/morrisjs/morris.min.js",
+     "~/Content/bootstrap/js/morris-data.js",
+      "~/Content/bootstrap/dist/js/sb-admin-2.js"
+      ));
+
+
+
         }
     }
 }
